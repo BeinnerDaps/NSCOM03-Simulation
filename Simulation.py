8fd16eeddda449ee19d305fb82db1f4587667889
BienMags <bienmagdamo2021@gmail.com>
2024-03-15 22:02:15 +0800

    Updated frequency

--- Simulation.py
+++ Simulation.py
@@ -4,74 +4,79 @@ import matplotlib.pyplot as plt
 
 def ASK(m):
     Tb = 1
-    fc = 10
-    t = np.arange(0, 1 + Tb/100, Tb/100)
+    fc = 50
+    sampling_rate = fc * 2 * 10
+    t = np.arange(0, Tb, Tb/sampling_rate)
     c = np.sqrt(2/Tb) * np.sin(2 * np.pi * fc * t)
 
     # generate message signal
-    N = 8
-    t1, t2 = 0, Tb
-
-    plt.figure("ASK")
+    N = len(m)
+    plt.figure("ASK", figsize=(15, 8))
 
     for i in range(N):
-        t = np.arange(t1, t2 + 0.01, 0.01)
-        if m[i] > 0.5:
-            m[i] = 1
-            m_s = np.ones(len(t))
-        else:
-            m[i] = 0
-            m_s = np.zeros(len(t))
+        t = np.arange(i*Tb, (i+1)*Tb, Tb/sampling_rate)
+        m_s = np.ones(len(t)) if m[i] == 1 else np.zeros(len(t))
 
         message = np.tile(m_s, (N, 1))
         ask_sig = c*m_s
 
-        t1 = t1+(Tb+ 0.01)
-        t2 = t2+(Tb+ 0.01)
+        # Plot the message and ASK signal
+        plt.subplot(4,1,2)
+        plt.axis([0, N, -2, 2])
+        plt.plot(t,message[i,:],'r')
+        plt.title('message signal')
+        plt.xlabel('t--->')
+        plt.ylabel('m(t)')
+        plt.grid(True)
 
-        plt.subplot(2,1,2)
+        plt.subplot(4,1,4)
         plt.plot(t, ask_sig)
         plt.title('ASK signal')
         plt.xlabel('t--->')
         plt.ylabel('s(t)')
         plt.grid(True)
 
-    # Plot the input binary data
-    plt.subplot(2,1,1)
+    # Plot the carrier signal and input binary data
+    plt.subplot(4,1,3)
+    plt.plot(t, c)
+    plt.title('carrier signal')
+    plt.xlabel('t--->')
+    plt.ylabel('c(t)')
+    plt.grid(True)
+
+    plt.subplot(4,1,1)
     plt.stem(m)
     plt.title('binary data bits')
     plt.xlabel('n--->')
+    plt.ylabel('b(n)')
     plt.grid(True)
 
-    plt.ylabel('b(n)')
     plt.tight_layout()
     plt.show()
+
 
 def FSK (m):
     # generate carrier signal
     Tb = 1
-    fc1, fc2 = 2, 5
-    t = np.arange(0, Tb + Tb/100, Tb/100)
+    fc1, fc2 = 25, 50
+    sampling_rate = max(fc1, fc2) * 2 * 10
+    t = np.arange(0, Tb, Tb/sampling_rate)
     c1 = np.sqrt(2/Tb) * np.sin(2 * np.pi * fc1 * t)
     c2 = np.sqrt(2/Tb) * np.sin(2 * np.pi * fc2 * t)
 
     # generate message signal
-    N = 8
-    t1, t2 = 0, Tb
-
+    N = len(m)
     message = np.zeros((N, len(t)))
     fsk = np.zeros((N, len(t)))
 
-    plt.figure("FSK")
+    plt.figure("FSK", figsize=(15, 8))
 
     for i in range(N):
-        t = np.arange(t1, t2 + Tb/100, Tb/100)
-        if m[i] > 0.5:
-            m[i] = 1
+        t = np.arange(i*Tb, (i+1)*Tb, Tb/sampling_rate)
+        if m[i] == 1:
             m_s = np.ones(len(t))
             invm_s = np.zeros(len(t))
         else:
-            m[i] = 0
             m_s = np.zeros(len(t))
             invm_s = np.ones(len(t))
 
@@ -82,31 +87,49 @@ def FSK (m):
         fsk[i, :] = fsk_sig1 + fsk_sig2
 
         # plotting the message signal and the modulated signal
-        plt.subplot(2,1,1)
+        plt.subplot(5,1,2)
+        plt.axis([0, N, -2, 2])
+        plt.plot(t,message[i,:], 'r')
+        plt.title('message signal')
+        plt.xlabel('t---->')
+        plt.ylabel('m(t)')
+        plt.grid(True)
+
+        plt.subplot(5,1,5)
         plt.plot(t,fsk[i,:])
         plt.title('FSK signal')
         plt.xlabel('t---->')
         plt.ylabel('s(t)')
         plt.grid(True)
 
-        t1 = t1+(Tb+ 0.01)
-        t2 = t2+(Tb+ 0.01)
-
     # plotting binary data bits and carrier signal
-    plt.subplot(2,1,2)
+    plt.subplot(5,1,1)
     plt.stem(m)
     plt.title('binary data')
     plt.xlabel('n---->')
     plt.ylabel('b(n)')
     plt.grid(True)
 
+    plt.subplot(5,1,3)
+    plt.plot(t, c1)
+    plt.title('carrier signal-1')
+    plt.xlabel('t---->')
+    plt.ylabel('c1(t)')
+    plt.grid(True)
+
+    plt.subplot(5,1,4)
+    plt.plot(t, c2)
+    plt.title('carrier signal-2')
+    plt.xlabel('t---->')
+    plt.ylabel('c2(t)')
+    plt.grid(True)
+
     plt.tight_layout()
     plt.show()
 
-
 m = input("Enter input signal:  ")
-while set(m).issubset({'0', '1'}) is False or len(list(m)) < 8:
-    print("Error: Linecode should be atleast 8-bit binary")
+while set(m).issubset({'0', '1'}) is False:
+    print("Error: Linecode should be binary")
     m = input("Enter linecode: ")
 
 choose = input("Choose input signal {ASK/FSK}: ")
