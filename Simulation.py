1c3ac360f75ced86bf308db0063543bf859eed2f
BienMags <bienmagdamo2021@gmail.com>
2024-03-17 12:02:48 +0800

    Cleaned up code to only use carrier signals and ASK/FSK signals

--- Simulation.py
+++ Simulation.py
@@ -3,52 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def ASK(m):
+    N = len(m)
     Tb = 1
     fc = 50
-    sampling_rate = fc * 2 * 10
-    t = np.arange(0, Tb, Tb/sampling_rate)
+    rate = fc * 2 * 10
+    t = np.arange(0, Tb, Tb/rate)
     c = np.sqrt(2/Tb) * np.sin(2 * np.pi * fc * t)
 
-    # generate message signal
-    N = len(m)
-    plt.figure("ASK", figsize=(15, 8))
-
+    plt.figure("ASK", figsize=(20, 7))
     for i in range(N):
-        t = np.arange(i*Tb, (i+1)*Tb, Tb/sampling_rate)
-        m_s = np.ones(len(t)) if m[i] == 1 else np.zeros(len(t))
+        t = np.arange(i*Tb, (i+1)*Tb, Tb/rate)
+        if m[i] == 1:
+            m_s = np.ones(len(t))
+        else:
+            m_s = np.zeros(len(t))
 
-        message = np.tile(m_s, (N, 1))
-        ask_sig = c*m_s
-
-        # Plot the message and ASK signal
-        plt.subplot(4,1,2)
-        plt.axis([0, N, -2, 2])
-        plt.plot(t,message[i,:],'r')
-        plt.title('message signal')
-        plt.xlabel('t--->')
-        plt.ylabel('m(t)')
-        plt.grid(True)
-
-        plt.subplot(4,1,4)
-        plt.plot(t, ask_sig)
+        # Plot ASK signal
+        plt.subplot(2,1,2)
+        plt.plot(t, c*m_s)
         plt.title('ASK signal')
         plt.xlabel('t--->')
         plt.ylabel('s(t)')
         plt.grid(True)
 
-    # Plot the carrier signal and input binary data
-    plt.subplot(4,1,3)
+    # Plot the carrier signal
+    plt.subplot(2,1,1)
     plt.plot(t, c)
     plt.title('carrier signal')
     plt.xlabel('t--->')
     plt.ylabel('c(t)')
-    plt.grid(True)
-
-    plt.subplot(4,1,1)
-    plt.stem(m)
-    plt.title('binary data bits')
-    plt.xlabel('n--->')
-    plt.ylabel('b(n)')
     plt.grid(True)
 
     plt.tight_layout()
@@ -56,23 +39,18 @@ def ASK(m):
 
 
 def FSK (m):
-    # generate carrier signal
+    N = len(m)
     Tb = 1
     fc1, fc2 = 25, 50
-    sampling_rate = max(fc1, fc2) * 2 * 10
-    t = np.arange(0, Tb, Tb/sampling_rate)
+    rate = max(fc1, fc2) * 2 * 10
+    t = np.arange(0, Tb, Tb/rate)
     c1 = np.sqrt(2/Tb) * np.sin(2 * np.pi * fc1 * t)
     c2 = np.sqrt(2/Tb) * np.sin(2 * np.pi * fc2 * t)
-
-    # generate message signal
-    N = len(m)
-    message = np.zeros((N, len(t)))
     fsk = np.zeros((N, len(t)))
 
-    plt.figure("FSK", figsize=(15, 8))
-
+    plt.figure("FSK", figsize=(15, 7))
     for i in range(N):
-        t = np.arange(i*Tb, (i+1)*Tb, Tb/sampling_rate)
+        t = np.arange(i*Tb, (i+1)*Tb, Tb/rate)
         if m[i] == 1:
             m_s = np.ones(len(t))
             invm_s = np.zeros(len(t))
@@ -80,44 +58,27 @@ def FSK (m):
             m_s = np.zeros(len(t))
             invm_s = np.ones(len(t))
 
-        message[i, :] = m_s
-
         fsk_sig1 = c1 * m_s
         fsk_sig2 = c2 * invm_s
         fsk[i, :] = fsk_sig1 + fsk_sig2
 
-        # plotting the message signal and the modulated signal
-        plt.subplot(5,1,2)
-        plt.axis([0, N, -2, 2])
-        plt.plot(t,message[i,:], 'r')
-        plt.title('message signal')
-        plt.xlabel('t---->')
-        plt.ylabel('m(t)')
-        plt.grid(True)
-
-        plt.subplot(5,1,5)
+        # plotting the message signal
+        plt.subplot(3,1,3)
         plt.plot(t,fsk[i,:])
         plt.title('FSK signal')
         plt.xlabel('t---->')
         plt.ylabel('s(t)')
         plt.grid(True)
 
-    # plotting binary data bits and carrier signal
-    plt.subplot(5,1,1)
-    plt.stem(m)
-    plt.title('binary data')
-    plt.xlabel('n---->')
-    plt.ylabel('b(n)')
-    plt.grid(True)
-
-    plt.subplot(5,1,3)
+    # plotting carrier signals
+    plt.subplot(3,1,1)
     plt.plot(t, c1)
     plt.title('carrier signal-1')
     plt.xlabel('t---->')
     plt.ylabel('c1(t)')
     plt.grid(True)
 
-    plt.subplot(5,1,4)
+    plt.subplot(3,1,2)
     plt.plot(t, c2)
     plt.title('carrier signal-2')
     plt.xlabel('t---->')
