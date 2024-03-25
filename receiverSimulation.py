7405095f1ee3c7eaf47d6897780b7329886b4232
BienMags <bienmagdamo2021@gmail.com>
2024-03-26 00:43:34 +0800

    Added the demodulators

--- receiverSimulation.py
+++ receiverSimulation.py
@@ -9,38 +9,111 @@ def receive_message():
         HOST = socket.gethostbyname(socket.gethostname())
         PORT = 12345
         s.bind((HOST, PORT))
-
-        s.listen(5)
+        s.listen(1)
         print(f"Listening on {HOST}:{PORT}")
         conn, addr = s.accept()
         with conn:
             print(f"Connected by {addr}")
             while True:
                 try:
-                    data = conn.recv(4096)
+                    data = conn.recv(4)
                     if not data:
                         break
-                    array.append(pickle.loads(data))
-                    print(f"Received array: {array}")
-                    # ASK_demod(array[0],array[1],array[2],array[3])
+                    data_len = int.from_bytes(data, 'big')
+                    received = b''
+                    while len(received) < data_len:
+                        chunk = conn.recv(data_len-len(received))
+                        if not chunk:
+                            raise ValueError("Could not complete transfer")
+                        received += chunk
+                    array.extend(pickle.loads(received))
                 except EOFError as e:
                     break
                 except Exception as e:
                     print(f"Exception:{e}")
                     break
 
-# def ASK_demod(fc,Tb,rate,ask_signal):
-#     N = len(ask_signal) // (rate * Tb)
-#     t = np.arange(0, N*Tb, Tb/rate)
-#     c = []
-#     for i in fc:
-#         c.append(np.sqrt(2/Tb)*np.sin(2*np.pi*fc[i]*t))
+        if array[0] == "ASK":
+            ASK_demod(array[1],array[2],array[3],array[4])
+        elif array[0] == "FSK":
+            FSK_demod(array[1],array[2],array[3],array[4])
 
-#     # Coherent Detection
-#     msg_sig = ask_signal * c
+def ASK_demod(fc,Tb,rate,ask_signal):
+    m,c = [],[]
+    N = len(ask_signal)
+    t = np.arange(0, Tb, Tb/rate)
+    c = np.sqrt(2/Tb)*np.sin(2*np.pi*fc[0]*t)
 
-#     lp_signal = np.zeros(N)
-#     for i in range(N):
-#         lp_signal[i] = np.mean(msg_sig[i*rate:(i+1)*rate])
+    for i in range(N):
+        t = np.arange(i*Tb, (i+1)*Tb, Tb/rate)
+        sig = ask_signal[i,:]*c
+        m.append(1) if sum(sig) > max(sig) else m.append(0)
+
+        #Plot ASK signal
+        plt.figure("ASK", figsize=(10, 7))
+        plt.subplot(2,1,2)
+        plt.plot(t, ask_signal[i,:])
+        plt.title('ASK signal')
+        plt.xlabel('t--->')
+        plt.ylabel('s(t)')
+        plt.grid(True)
+
+    m = np.array(m)
+    print(m)
+
+    # Plotting carrier signal
+    plt.subplot(2,1,1)
+    plt.plot(t, c)
+    plt.title('carrier signal')
+    plt.xlabel('t--->')
+    plt.ylabel('c(t)')
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
+def FSK_demod(fc,Tb,rate,fsk_signal):
+    m = []
+    N = len(fsk_signal)
+    t = np.arange(0, Tb, Tb/rate)
+    c1 = np.sqrt(2/Tb)*np.sin(2*np.pi*fc[0]*t) # 25
+    c2 = np.sqrt(2/Tb)*np.sin(2*np.pi*fc[1]*t) # 50
+
+    for i in range(N):
+        t = np.arange(i*Tb, (i+1)*Tb, Tb/rate)
+        segment = fsk_signal[i,:]
+        co1 = segment * c1[:len(segment)]
+        co0 = segment * c2[:len(segment)]
+        m.append(1) if np.sum(co1) > np.sum(co0) else m.append(0)
+
+        # Plot FSK signal
+        plt.figure("ASK", figsize=(10, 7))
+        plt.subplot(3,1,3)
+        plt.plot(t,fsk_signal[i,:])
+        plt.title('FSK signal')
+        plt.xlabel('t---->')
+        plt.ylabel('s(t)')
+        plt.grid(True)
+
+    m = np.array(m)
+    print(m)
+
+    # Plotting carrier signals
+    plt.subplot(3,1,1)
+    plt.plot(t, c1)
+    plt.title('carrier signal-1')
+    plt.xlabel('t---->')
+    plt.ylabel('c1(t)')
+    plt.grid(True)
+
+    plt.subplot(3,1,2)
+    plt.plot(t, c2)
+    plt.title('carrier signal-2')
+    plt.xlabel('t---->')
+    plt.ylabel('c2(t)')
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.show()
 
 receive_message()
--- senderSimulation.py
+++ senderSimulation.py
@@ -7,58 +7,32 @@ import socket
 def ASK_mod(m):
     N = len(m)
     Tb = 1
-    fc = []
-    fc.append(50)
+    fc = [50]
     rate = fc[0] * 2 * 10
     t = np.arange(0, Tb, Tb/rate)
     c = np.sqrt(2/Tb) * np.sin(2 * np.pi * fc[0] * t)
-
-    plt.figure("ASK", figsize=(10, 7))
     ask_signal = np.zeros((N, len(t)))
+
     for i in range(N):
         t = np.arange(i*Tb, (i+1)*Tb, Tb/rate)
         if m[i] == 1:
             m_s = np.ones(len(t))
         else:
             m_s = np.zeros(len(t))
-
         ask_signal[i,:] = c*m_s
-        # print(ask_signal[i*(len(t)//len(m)):(i+1)*(len(t)//len(m))])
 
-        # Plot ASK signal
-        plt.subplot(2,1,2)
-        plt.plot(t, ask_signal[i,:])
-        plt.title('ASK signal')
-        plt.xlabel('t--->')
-        plt.ylabel('s(t)')
-        plt.grid(True)
-
-    # Plot the carrier signal
-    plt.subplot(2,1,1)
-    plt.plot(t, c)
-    plt.title('carrier signal')
-    plt.xlabel('t--->')
-    plt.ylabel('c(t)')
-    plt.grid(True)
-
-    transmit_message(fc, Tb, rate, ask_signal)
-
-    plt.tight_layout()
-    # plt.show()
+    transmit_message("ASK", fc, Tb, rate, ask_signal)
 
 def FSK_mod(m):
     N = len(m)
     Tb = 1
-    fc = []
-    fc.append(25)
-    fc.append(50)
+    fc = [25, 50]
     rate = max(fc[0], fc[1]) * 2 * 10
     t = np.arange(0, Tb, Tb/rate)
     c1 = np.sqrt(2/Tb) * np.sin(2 * np.pi * fc[0] * t)
     c2 = np.sqrt(2/Tb) * np.sin(2 * np.pi * fc[1] * t)
     fsk_signal = np.zeros((N, len(t)))
 
-    plt.figure("FSK", figsize=(15, 7))
     for i in range(N):
         t = np.arange(i*Tb, (i+1)*Tb, Tb/rate)
         if m[i] == 1:
@@ -67,63 +41,32 @@ def FSK_mod(m):
         else:
             m_s = np.zeros(len(t))
             invm_s = np.ones(len(t))
-
         fsk_sig1 = c1 * m_s
         fsk_sig2 = c2 * invm_s
         fsk_signal[i,:] = fsk_sig1 + fsk_sig2
 
-        # plotting the message signal
-        plt.subplot(3,1,3)
-        plt.plot(t,fsk_signal[i,:])
-        plt.title('FSK signal')
-        plt.xlabel('t---->')
-        plt.ylabel('s(t)')
-        plt.grid(True)
+    transmit_message("FSK", fc, Tb, rate, fsk_signal)
 
-    # plotting carrier signals
-    plt.subplot(3,1,1)
-    plt.plot(t, c1)
-    plt.title('carrier signal-1')
-    plt.xlabel('t---->')
-    plt.ylabel('c1(t)')
-    plt.grid(True)
-
-    plt.subplot(3,1,2)
-    plt.plot(t, c2)
-    plt.title('carrier signal-2')
-    plt.xlabel('t---->')
-    plt.ylabel('c2(t)')
-    plt.grid(True)
-
-    plt.tight_layout()
-    plt.show()
-
-    transmit_message(fc, Tb, rate, fsk_signal)
-
-def transmit_message(fc, Tb, rate, mod_sig):
+def transmit_message(sig,fc, Tb, rate, mod_sig):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         HOST = socket.gethostbyname(socket.gethostname())
         PORT = 12345
         s.connect((HOST, PORT))
-        send = [fc,Tb,rate]
+        send = [sig,fc,Tb,rate,mod_sig]
         send_pickle = pickle.dumps(send)
-        s.sendall(send_pickle)
-        for i in mod_sig:
-            send_pickle = pickle.dumps(i)
-            s.sendall(len(send_pickle).to_bytes(4,'big') + send_pickle)
+        s.sendall(len(send_pickle).to_bytes(4,'big') + send_pickle)
         s.close()
 
-# m = input("Enter input signal:  ")
-# while set(m).issubset({'0', '1'}) is False:
-#     print("Error: Linecode should be binary")
-#     m = input("Enter linecode: ")
+m = input("Enter input signal:  ")
+while set(m).issubset({'0', '1'}) is False:
+    print("Error: Linecode should be binary")
+    m = input("Enter linecode: ")
 
-# choose = input("Choose input signal {ASK/FSK}: ")
-# while choose not in ('ASK', 'FSK'):
-#     print("Error: Signals should be ASK or FSK")
-#     choose = input("Choose input signal: (ASK/FSK)")
-m = "010101011"
-choose = "ASK"
+choose = input("Choose input signal {ASK/FSK}: ")
+while choose not in ('ASK', 'FSK'):
+    print("Error: Signals should be ASK or FSK")
+    choose = input("Choose input signal: (ASK/FSK)")
+
 m = np.array(list(map(int, m)))
 if choose == "ASK":
     ASK_mod(m)
